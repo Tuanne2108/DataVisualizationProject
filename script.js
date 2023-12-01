28fd66d81817ec2b9b33c300c9f34e31f0c73706
Tuanne2108 <107233041+Tuanne2108@users.noreply.github.com>
2023-12-01 19:29:23 +0700

    Handle function for slider to update charts for a selected year. Show the value on the card and Quantity. Next, i will make a legends for pie chart and replace the name of country with percentages

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ var rowConverter = (d) => {
     };
 };
 
-function barChart() {
+function barChart(selectedYear) {
     d3.csv(
         "https://gist.githubusercontent.com/Tuanne2108/d10d78e8e1533927347d3ce2d04c714f/raw/97638632aef8770337415ce610cc7a14ce67e2dc/covid19_confirmed.csv",
         rowConverter
     ).then((data) => {
-        let filteredData = data.filter((d) => d.year === 1995);
+        let filteredData = data.filter((d) => d.year === selectedYear);
         let dataset = Array.from(d3.groups(filteredData, (d) => d.country));
         let countries = Object.values(dataset).map((arr) => arr[0]);
 
@@ -35,42 +35,28 @@ function barChart() {
             .domain([0, maxValue * 1.2])
             .range([height - padding, padding]);
 
-        let svg = d3
-            .select(".bar-chart")
-            .append("svg")
-            .attr("height", height)
-            .attr("width", width);
+        let svg = d3.select(".bar-chart svg");
+        if (svg.empty()) {
+            svg = d3
+                .select(".bar-chart")
+                .append("svg")
+                .attr("height", height)
+                .attr("width", width);
+        }
 
-        svg.selectAll("rect")
-            .data(countries)
+        // Update rectangles
+        let rects = svg.selectAll("rect").data(countries);
+        
+        // Remove rectangles that are no longer needed
+        rects.exit().remove(); 
+
+        rects
             .enter()
             .append("rect")
-            .attr("x", function (d) {
-                return xScale(d);
-            })
-            .attr("y", (d) =>
-                yScale(
-                    d3.sum(
-                        dataset.find((entry) => entry[0] === d)[1],
-                        (d) => d.cases
-                    )
-                )
-            )
             .attr("width", xScale.bandwidth())
-            .attr("height", function (d) {
-                return (
-                    height -
-                    padding -
-                    yScale(
-                        d3.sum(
-                            dataset.find((entry) => entry[0] === d)[1],
-                            (d) => d.cases
-                        )
-                    )
-                );
-            })
-            .attr("fill", function (d) {
-                return (
+            .attr(
+                "fill",
+                (d) =>
                     "rgb(47, " +
                     yScale(
                         d3.sum(
@@ -79,69 +65,112 @@ function barChart() {
                         )
                     ) +
                     ",120)"
-                );
-            });
+            )
+            .merge(rects)
+            .transition()
+            .duration(500)
+            .attr("x", (d) => xScale(d))
+            .attr("y", (d) =>
+                yScale(
+                    d3.sum(
+                        dataset.find((entry) => entry[0] === d)[1],
+                        (d) => d.cases
+                    )
+                )
+            )
+            .attr(
+                "height",
+                (d) =>
+                    height -
+                    padding -
+                    yScale(
+                        d3.sum(
+                            dataset.find((entry) => entry[0] === d)[1],
+                            (d) => d.cases
+                        )
+                    )
+            );
 
-        // Add x-axis
-        let xAxis = d3.axisBottom().scale(xScale);
-        svg.append("g")
-            .attr("class", "xAxis")
-            .attr("transform", "translate(0," + (height - padding) + ")")
-            .attr("font-size", 16)
-            .call(xAxis)
+        // Update x-axis
+        let xAxis = svg.select(".xAxis");
+        if (xAxis.empty()) {
+            xAxis = svg
+                .append("g")
+                .attr("class", "xAxis")
+                .attr("transform", "translate(0," + (height - padding) + ")");
+        }
+        xAxis
+            .transition()
+            .duration(500)
+            .call(d3.axisBottom().scale(xScale))
             .selectAll("text")
             .attr("transform", "rotate(-25)")
             .attr("text-anchor", "end")
             .style("font-weight", "bold");
 
-        // Add y-axis
-        let yAxis = d3.axisLeft().scale(yScale);
-        svg.append("g")
-            .attr("class", "yAxis")
-            .attr("transform", "translate(" + padding + ",0)")
-            .attr("font-size", 16)
-            .call(yAxis);
+        // Update y-axis
+        let yAxis = svg.select(".yAxis");
+        if (yAxis.empty()) {
+            yAxis = svg
+                .append("g")
+                .attr("class", "yAxis")
+                .attr("transform", "translate(" + padding + ",0)");
+        }
+        yAxis.transition().duration(500).call(d3.axisLeft().scale(yScale));
 
-        svg.append("text")
-            .text("Case(s)")
-            .attr("class", "axis-label")
-            .attr("text-anchor", "middle")
-            .attr("x", -height / 2)
-            .attr("y", padding / 2)
-            .attr("fill", "black")
-            .attr("font-size", 20)
-            .attr("transform", "rotate(-90)");
-
-        // Add chart title
-        svg.append("text")
-            .text("Confirmed Cases for Each Country in 1995")
-            .attr("class", "axis-label")
-            .attr("text-anchor", "middle")
-            .attr("x", width / 2)
-            .attr("y", padding * 0.6)
-            .attr("fill", "black")
-            .attr("font-size", 24);
+        // Update chart title
+        let chartTitle = svg.select(".axis-label");
+        if (chartTitle.empty()) {
+            chartTitle = svg
+                .append("text")
+                .attr("class", "axis-label")
+                .attr("text-anchor", "middle")
+                .attr("x", width / 2)
+                .attr("y", padding * 0.6)
+                .attr("fill", "black")
+                .attr("font-size", 24);
+        }
+        chartTitle.text("Confirmed Cases for Each Country in " + selectedYear);
+        const totalCases = d3.sum(
+            dataset.flatMap((entry) => entry[1]),
+            (d) => d.cases
+        );
+        const formatNumber = d3.format(",")(totalCases);
+        document.querySelector(".amount-cases").textContent = formatNumber;
     });
+    pieChart(selectedYear);
 }
 
-//Create pie chart
-function pieChart() {
+function pieChart(selectedYear) {
     d3.csv(
         "https://gist.githubusercontent.com/Tuanne2108/d10d78e8e1533927347d3ce2d04c714f/raw/97638632aef8770337415ce610cc7a14ce67e2dc/covid19_confirmed.csv",
         rowConverter
     ).then((data) => {
-        let yearData = data.filter((d) => d.year === 1995);
+        let yearData = data.filter((d) => d.year === selectedYear);
 
-        let svg = d3
-            .select(".pie-chart")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .append("g")
-            .attr(
-                "transform",
-                "translate(" + width / 2.7 + "," + height / 2 + ")"
-            );
+        // Select existing SVG element if it exists, otherwise create a new one
+        let svg = d3.select(".pie-chart svg");
+        if (svg.empty()) {
+            svg = d3
+                .select(".pie-chart")
+                .append("svg")
+                .attr("width", width)
+                .attr("height", height)
+                .append("g")
+                .attr(
+                    "transform",
+                    "translate(" + width / 2 + "," + height / 2 + ")"
+                );
+        } else {
+            // Clear existing content when updating
+            svg.selectAll("*").remove();
+            svg = svg
+                .append("g")
+                .attr(
+                    "transform",
+                    "translate(" + width / 2.65 + "," + height / 2 + ")"
+                );
+        }
 
         const color = d3.scaleOrdinal(d3.schemeCategory10);
 
@@ -160,7 +189,6 @@ function pieChart() {
             .attr("fill", (d, i) => color(i))
             .attr("d", arc);
 
-        // Optional: Add labels
         arcs.append("text")
             .attr("transform", (d) => "translate(" + arc.centroid(d) + ")")
             .attr("text-anchor", "middle")
@@ -170,9 +198,44 @@ function pieChart() {
             .text("Ratio of categories")
             .attr("class", "axis-label")
             .attr("text-anchor", "middle")
-            .attr("x", width / 100)
-            .attr("y", padding * -1.5)
+            .attr("x", 0)
+            .attr("y", -height / 4 - padding / 2)
             .attr("fill", "black")
             .attr("font-size", 24);
     });
 }
+
+// Handle data
+document.addEventListener("DOMContentLoaded", function () {
+    const slider = document.getElementById("year");
+
+    // Fetch data and update slider when the page loads
+    d3.csv(
+        "https://gist.githubusercontent.com/Tuanne2108/d10d78e8e1533927347d3ce2d04c714f/raw/97638632aef8770337415ce610cc7a14ce67e2dc/covid19_confirmed.csv",
+        rowConverter
+    ).then((data) => {
+        const uniqueYears = [...new Set(data.map((d) => d.year))];
+        const yearNumber = document.querySelector(".year-number");
+
+        // Set slider attributes
+        slider.min = uniqueYears[0];
+        slider.max = uniqueYears[uniqueYears.length - 1];
+        slider.value = uniqueYears[0];
+        yearNumber.textContent = uniqueYears[0];
+
+        // Call the barChart function with the selected year
+        barChart(uniqueYears[0]);
+
+
+        slider.addEventListener("input", function () {
+            // Get the current value of the slider
+            const sliderValue = +this.value;
+
+            // Call the barChart function with the selected year
+            barChart(sliderValue);
+
+            // Update the content of the element with class 'year-number'
+            yearNumber.textContent = sliderValue;
+        });
+    });
+});
