dec9b7fc8a8704a9f896ebbfe1af7ea5f17b6adb
TonThatMinhVu <ITITIU20356@student.hcmiu.edu.vn>
2023-12-20 13:07:59 +0700

    1

--- main.js
+++ main.js
@@ -1,111 +1,110 @@
-//chart dimensions 
-const margin ={top: 70, right:40, bottom:60, left: 175}
-const width =600 - margin.left - margin.right
-const height = 400 - margin.top -margin.bottom
-// svg container
-const svg = d3.select("#Grouped bar").append("svg")
-.attr("width",width + margin.left + margin.right)
-.attr("height", height+ margin.top + margin.bottom)
-.append("g")
-.attr("transform" , "translate( "+ margin.left + "'" + margin.top+")");
-//Load data
-d3.cssv("nhets file csv vo.csv").then (data =>{
-    data.forEach(d=>{
-        d.total =+d.total;
-    });
+// Chart dimensions
+const margin = { top: 70, right: 40, bottom: 60, left: 175 };
+const width = 600 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
 
-// Sort data by total
-    data.sort(funtion(a,b) {
-        return d3.ascending(a.total, b.total); 
-    });
-//set x y scales
-const x = d3.scaleLinear()
-    .range([0,width])
-    .domain([0, d3.max(data, function (d){return d.total; })]);
+// SVG container
+const svg = d3
+  .select("#GroupedBarChart")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    const y = d3.scaleBand()
-    .range([height, 0])
-    .padding(0.1)
-    .domain(data.map(function(d){ return d.Groupedbar_body_type}));
-    //create axis
-    const x Axis =d3.axisBottom(x)
-    .tick(5)
-    .tickSize(0); //remove ticks
-    const y Axis =d3.axisLeft(y)
-    .tickSize(0)
-    .tickPadding(10);
-    //add x y to the chart
-    svg.append("g")
-    .attr("class","x axis")
-    .style("font-size","10px")
-    .attr("transform","translate(0," + height +")")
-    .call(xAxis)
-    .call(g => g.select(".domain").remove());
+d3.csv("https://raw.githubusercontent.com/Tuanne2108/DataVisualizationProject/main/Global_Education.csv").then(data => {
+  // Assuming your CSV file contains appropriate data columns, you can proceed to create the grouped bar chart.
 
-    svg.append("g")
-    .attr("class","y axis")
-    .style("font-size","8px")
-    .call(yAxis)
-    .selectAll('path')
-    .style('stroke-width','1.75px');
-//Create bars for chart
-svg.selectAll(".bar")
-.data(data)
-.enter().append("rect")
-.attr("class","bar")
-.attr("y",function (d){ return y(d.Groupedbar_body_type);})
-.attr("height",y.bandwidth())
-.attr("x",0)
-.attr("width",function(d){return x(d.total);})
-.style("fill",'skyblue')
-// add vertical gridlines
-svg.selectAll("line.vertical-grid")
-.data(x.ticks(5))
-.enter()
-.append("line")
-.attr("class", "vertical-grid")
-.attr("x1",function (d) {return x(d); })
-.attr("y1",0)
-.attr("x2",function (d) {return x(d); })
-.attr("y2",height)
-.style("stroke","gray")
-.style("stroke-width",0.5)
-.style("stroke-dasharray","3 3");
-//Add labels to each bar
-svg.selectAll(".label")
-.data(data)
-.enter().append("text")
-.attr("x", function (d){ return x(d.total)+5; })
-.attr("y", function (d) { return y(d.Groupedbar_body_type)+ y.bandwidth() / 2;})
-.attr("dy", ".35em")
-.style("font-family","sans-serif")
-.style("font-size","10px")
-.style("font-weight", "bold")
-.style('fill', '#3c3d28')
-.text(function (d){ return d,total;});
-// Add total label
-svg.append("text")
-.attr("transform", "translate(" +width /2 + "," + (height +margin.bottom))
-.style("text-anchor","middle")
-.style("font-size","10px")
-.style("fill","black")
-.style("font-family","sans-serif")
-.attr("dy","1em")
-.text("Total");
-// Add chart title
-svg.append("text")
-.attr("x",margin.left -335)
-.attr("y", margin.top - 110)
-.style("font-size","14px")
-.style("font-weight","bold")
-.style("font-family","sans-serif")
-.text("cai tieu de");
+  // Create a list of all unique Groupedbar_body_type values
+  const groupNames = [...new Set(data.map(d => d.Groupedbar_body_type))];
 
-// Add the chart data source
-svg.append("text")
-.attr("transform","translate("+(margin.left - 335)+ "," +(height + margin.bottom))
-.style("text-anchor","start")
-.style("font-size","8px")
-.style("fill","lightgray")
-.style("font-family","san-serif")
-.html("nhet cai link vo day")
+  // Create color scale for the bars
+  const colorScale = d3.scaleOrdinal()
+    .domain(groupNames)
+    .range(d3.schemeCategory10);
+
+  // Stack the data for grouped bars
+  const stackedData = d3.stack()
+    .keys(groupNames)
+    .order(d3.stackOrderNone)
+    .offset(d3.stackOffsetNone)(data);
+
+  // Set x and y scales
+  const x = d3.scaleBand()
+    .domain(data.map(d => d.Country)) // Change 'xAxisLabel' to the actual data column you want on the x-axis
+    .range([0, width])
+    .padding(0.1);
+
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(stackedData[stackedData.length - 1], d => d[1])]) // Adjust the domain as needed
+    .nice()
+    .range([height, 0]);
+
+  // Create x and y axes
+  const xAxis = d3.axisBottom(x);
+  const yAxis = d3.axisLeft(y);
+
+  // Add x and y axes to the chart
+  svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+  svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
+
+  // Create groups for the bars
+  const groups = svg.selectAll(".group")
+    .data(stackedData)
+    .enter().append("g")
+    .attr("class", "group")
+    .style("fill", d => colorScale(d.key));
+
+  // Create bars within each group
+  groups.selectAll("rect")
+    .data(d => d)
+    .enter().append("rect")
+    .attr("x", d => x(d.data.xAxisLabel))
+    .attr("y", d => y(d[1]))
+    .attr("height", d => y(d[0]) - y(d[1]))
+    .attr("width", x.bandwidth());
+
+  // Add legend
+  const legend = svg.selectAll(".legend")
+    .data(groupNames.reverse()) // Reverse to match the order of bars
+    .enter().append("g")
+    .attr("class", "legend")
+    .attr("transform", (d, i) => "translate(0," + i * 20 + ")");
+
+  legend.append("rect")
+    .attr("x", width - 18)
+    .attr("width", 18)
+    .attr("height", 18)
+    .style("fill", colorScale);
+
+  legend.append("text")
+    .attr("x", width - 24)
+    .attr("y", 9)
+    .attr("dy", ".35em")
+    .style("text-anchor", "end")
+    .text(d => d);
+
+  // Add chart title
+  svg.append("text")
+    .attr("x", width / 2)
+    .attr("y", -30)
+    .attr("text-anchor", "middle")
+    .style("font-size", "16px")
+    .style("font-weight", "bold")
+    .text("Your Chart Title");
+
+  // Add the chart data source
+  svg.append("text")
+    .attr("x", width / 2)
+    .attr("y", height + margin.bottom - 10)
+    .attr("text-anchor", "middle")
+    .style("font-size", "10px")
+    .style("fill", "lightgray")
+    .html("Data Source: <a href='https://raw.githubusercontent.com/Tuanne2108/DataVisualizationProject/main/Global_Education.csv' target='_blank'>https://raw.githubusercontent.com/Tuanne2108/DataVisualizationProject/main/Global_Education.csv</a>");
+});
